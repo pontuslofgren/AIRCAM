3fd2ad765124a0814456de092e430f8ffe8626c4
Pontus Lofgren <pontus.loefgren@gmail.com>
2023-11-21 18:07:40 +0000

    Added custom validator: start date must be earlier than end date

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :cameras
+  resources :cameras do
+    member do
+      # Availability must have camera_id as a param
+      # Availability only has new, create and destroy
+      resources :availabilities, only: [:new, :create, :destroy]
+    end
+  end
 end
